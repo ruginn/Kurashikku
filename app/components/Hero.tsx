025474c1f02882ff76c5a3fcb0a4fcf3cb696d09
Rcatarata <Rcatarata@gmail.com>
2023-11-01 21:09:46 -0400

    hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,17 +1,23 @@
 import { cn } from "@/lib/utils"
 import Image from 'next/image'
 import Sushi from '../../public/sushi1.jpg'
+import { Button } from "@/components/ui/button"
 
 const Hero = () => {
     return (
-        <div className={cn('h-[calc(100vh-4rem)] bg-black flex justify-center items-center relative')}>
-            <Image
-                src={Sushi}
-                alt='sushi'
-                className="h-[600px] w-[1000px] object-cover rounded-2xl mb-32"
-            >
-            </Image>
-            <h1 className="text-white absolute text-[200px] right-36 select-none">倉珠玖</h1>
+        <div className={cn('h-[calc(100vh-4rem)] bg-black flex-col justify-center items-center')}>
+            <div className="flex relative justify-center items-center w-full ">
+                <Image
+                    src={Sushi}
+                    alt='sushi'
+                    className="h-[600px] w-[1000px] object-cover rounded-2xl mt-32"
+                >
+                </Image>
+                <h1 className="text-white absolute text-[200px] right-36 select-none">倉珠玖</h1> 
+            </div>
+            <div className="flex w-full justify-end">
+                <Button variant='secondary' className="mr-96 mt-16 px-16">Reservation</Button>
+            </div>
         </div>
     )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,21 @@
+import { SignInButton, UserButton, SignedIn, SignedOut } from "@clerk/nextjs"
+
 const Navbar = () => {
   return (
     <div className="w-full h-16 bg-slate-100 flex justify-between items-center">
-        <h1 className="text-2xl ml-8 font-medium">Kurashikku</h1>
+        <h1 className="text-2xl ml-8 font-medium">Kurashiku</h1>
         <ul className="flex mr-8">
             <button>
                 <li className="m-4">Create Reservation</li>
             </button>
             <button>
-                <li className="m-4">Login</li>
+                {/* <SignedIn>
+                  <UserButton />
+                </SignedIn>
+                <SignedOut>
+                 <SignInButton mode='modal'/>
+                </SignedOut> */}
+                <SignInButton mode='modal'/>
             </button>
         </ul>
     </div>

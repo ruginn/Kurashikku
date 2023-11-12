bd022346da939f13ba74268bc906d4bc962b6623
Rcatarata <Rcatarata@gmail.com>
2023-11-11 22:34:36 -0500

    Update ReservationModal.tsx

--- components/modal/ReservationModal.tsx
+++ components/modal/ReservationModal.tsx
@@ -2,7 +2,7 @@ import { useReservation } from "@/hooks/use-reservation"
 import { Dialog, DialogContent, DialogHeader} from "@/components/ui/dialog";
 import {useState, useEffect} from 'react'
 import { format } from "date-fns"
-import { Calendar as CalendarIcon, Clock, Users } from "lucide-react"
+import { Calendar as CalendarIcon, ChevronDown, Clock, Users } from "lucide-react"
  
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
@@ -24,55 +24,82 @@ import {
 
 const ReservationModal = () => {
     const reservation = useReservation();
-
+    const [resCon, setResCon] = useState(false)
     const [date, setDate] = useState<Date>()
     
+    const createReservation = () => {
+        
+        setResCon(true)
+        console.log('texting')
+    }
+    const months = [
+        "January", "February", "March", "April", 
+        "May", "June", "July", "August", 
+        "September", "October", "November", "December"
+      ];
+    
+    const [resMonth, setResMonth] = useState<String>('')
+
     useEffect(() => {
-      console.log(date)
-    }, [date])
+        if (date?.getMonth()){
+            setResMonth(months[date?.getMonth()]) 
+        }
+    },[date])
+
     
     const partySize = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    
+    const [partyVal, setPartyVal] = useState<String>()
+    
+
     const hours = [
         '5:30 PM', '6:00 PM', '6:30 PM', '7:00 PM', '7:30 PM', '8:00 PM',
         '8:30 PM', '9:00 PM', '9:30 PM', '10:00 PM', '10:30 PM', '11:00 PM', '11:30 PM'
       ]
     
+    const [time, setTime] = useState<String>()
+
+
+
     return (
         <Dialog open={reservation.isOpen} onOpenChange={reservation.onClose}>
-            <DialogContent>
+            {!resCon && <DialogContent>
                 <DialogHeader className="border-b pb-3">
                 <h2 className="text-lg font-medium">
                     Create a reservation
                 </h2>
                 </DialogHeader>
-                <Select>
-                <SelectTrigger className="w-full">
-                    <div className="flex flex-row items-center">
-                        <Users className="ml-1 h-4 w-4 mr-2 stroke-[2.5px]"/>
-                        <SelectValue placeholder="Party"/>
-                    </div>
-                    {/* <User className="h-4 w-4 stroke-[2.5px]"/>
-                    <SelectValue placeholder="Party" /> */}
-                </SelectTrigger>
-                <SelectContent>
-                    {partySize.map((party) => {
-                        return (
-                            <SelectItem key={party.toString()} value={party.toString()}>{party.toString()}</SelectItem>
-                        )
-                    })}
-                </SelectContent>
+                <Select onValueChange={setPartyVal}>
+                    <SelectTrigger className="w-full">
+                        <div className="flex flex-row items-center">
+                            <Users className="ml-1 h-4 w-4 mr-2 stroke-[2.5px]"/>
+                            <SelectValue placeholder="Party"/>
+                        </div>
+                        {/* <User className="h-4 w-4 stroke-[2.5px]"/>
+                        <SelectValue placeholder="Party" /> */}
+                    </SelectTrigger>
+                    <SelectContent>
+                        {partySize.map((party) => {
+                            return (
+                                <SelectItem key={party.toString()} value={party.toString()}>{party.toString()}</SelectItem>
+                            )
+                        })}
+                    </SelectContent>
                 </Select>
                 <Popover>
                     <PopoverTrigger asChild>
                         <Button
                         variant={"outline"}
                         className={cn(
-                            "w-full justify-start text-left font-normal",
+                            "w-full flex justify-between text-left font-normal",
                             // !date && "text-muted-foreground"
                         )}
                         >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date ? format(date, "PPP") : <span>Pick a date</span>}
+                        <div className="flex items-center">
+                            <CalendarIcon className="mr-2 h-4 w-4" />
+                            {date ? format(date, "PPP") : <span>Pick a date</span>}
+                        </div>
+                        <ChevronDown className="-mr-1 h-4 w-4 text-muted-foreground" />
                         </Button>
                     </PopoverTrigger>
                     <PopoverContent className="w-auto p-0">
@@ -84,7 +111,7 @@ const ReservationModal = () => {
                         />
                     </PopoverContent>
                 </Popover>
-                <Select>
+                <Select onValueChange={setTime}>
                 <SelectTrigger className="w-full">
                     <div className="flex flex-row items-center">
                         <Clock className="ml-1 h-4 w-4 mr-2 stroke-[2.5px]"/>
@@ -99,10 +126,22 @@ const ReservationModal = () => {
                     })}
                 </SelectContent>
                 </Select>
-                <Button className="mt-6">Book Reservation</Button>
-            </DialogContent>
+                <Button onClick={createReservation} className="mt-6">Book Reservation</Button>
+            </DialogContent>}
+            {resCon && 
+            <DialogContent>
+                <DialogHeader className="border-b pb-3">
+                <h2 className="text-lg font-medium">
+                    Reservation confirmed. 
+                </h2>
+                </DialogHeader>     
+                <div className="mb-16">{`Your reservation of ${partyVal} has been set for ${resMonth} ${date?.getDate()}, ${date?.getFullYear()} at ${time}.`}</div> 
+            </DialogContent>   
+            }
         </Dialog>
-    )
+    ) 
 }
 
 export default ReservationModal
+
+
